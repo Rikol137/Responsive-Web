9cb75f212b947033c23513566ee63291a5525e01
Rikol137 <rikolkelvin@gmail.com>
2022-10-12 12:16:45 +0300

    update html css and js

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+const projects = [
+  {
+    id: '0',
+    title: 'Hein',
+    tags: ['Canopy', 'Back End dev', '2015'],
+    imgsrc: 'images/project1.png',
+    description: {
+      small: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      big: 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    technologies: ['HTML', 'CSS', 'Ruby on Rails'],
+    links: ['https://github.com', 'https://heroku.com'],
+  },
+  {
+    id: '1',
+    title: 'Multi-Post Stories',
+    tags: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    imgsrc: 'images/project2.png',
+    description: {
+      small: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      big: 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    links: ['https://github.com', 'https://heroku.com'],
+  },
+  {
+    id: '2',
+    title: 'Tonic',
+    tags: ['Canopy', 'Back End dev', '2015'],
+    imgsrc: 'images/project3.png',
+    description: {
+      small: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      big: 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    technologies: ['HTML', 'CSS', 'Ruby on Rails'],
+    links: ['https://github.com', 'https://heroku.com'],
+  },
+  {
+    id: '3',
+    title: 'Tonic',
+    tags: ['Canopy', 'Back End dev', '2015'],
+    imgsrc: 'images/project4.png',
+    description: {
+      small: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      big: 'A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required. A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    },
+    technologies: ['HTML', 'CSS', 'Ruby on Rails'],
+    links: ['https://github.com', 'https://heroku.com'],
+  },
+];
+
 const navbarHamburger = document.querySelector('#navbar-hamburger');
 const mobileMenu = document.querySelector('#mobile-menu');
 const cross = document.querySelector('#cross');
@@ -16,3 +67,92 @@ document.querySelectorAll('.nav-link').forEach(
     });
   },
 );
+
+let listOfelements = '';
+let listProject = '';
+projects.forEach((project, i) => {
+  listOfelements = '';
+  project.technologies.forEach((technologie) => {
+    listOfelements += `<li class="tag">${technologie}</li>`;
+  });
+  listProject += `<div class="card">
+  <img class="snapshot" src="${project.imgsrc}" alt="" />
+  <div class="left-block ${i % 2 !== 0 ? 'right' : ''}">
+      <h1>${project.title}</h1>
+      <ul class="info">
+          <li class="title">${project.tags[0]}</li>
+          <li><img src="images/dot.png"></li>
+          <li class="description">${project.tags[1]}</li>
+          <li><img src="images/dot.png"></li>
+          <li class="description">${project.tags[2]}</li>
+      </ul>
+      <p>${project.description.small}</p>
+      <ul class="categories">
+         ${listOfelements}
+      </ul>
+      <div>
+          <button type="button" class="btn card-btn">See Project</button>
+      </div>
+  </div>
+  </div>`;
+});
+const workSection = document.querySelector('#works');
+workSection.innerHTML += `${listProject}`;
+const divPopup = document.createElement('div');
+
+const cardBtns = document.querySelectorAll('.card-btn');
+cardBtns.forEach((cardBtn, i) => {
+  cardBtn.addEventListener('click', () => {
+    divPopup.style.display = 'flex';
+    divPopup.classList.add('pop-up');
+    listOfelements = '';
+    projects[i].technologies.forEach((technologie) => {
+      listOfelements += `<li class="tag">${technologie}</li>`;
+    });
+    const popupText = `
+    <div class="pop-up-content">
+        <div class="left-block ">
+            <div class="pop-up-title">
+                <h2>${projects[i].title}</h2>
+                <img src="images/cross-dark.png" class="close-popup pointer" alt="cross-dark">
+            </div>
+            <ul class="info">
+                <li class="title">${projects[i].tags[0]}</li>
+                <li><img src="images/dot.png"></li>
+                <li class="description">${projects[i].tags[1]}</li>
+                <li><img src="images/dot.png"></li>
+                <li class="description">${projects[i].tags[2]}</li>
+            </ul>
+            <img class="snapshot" src="${projects[i].imgsrc}" alt="" />
+            <div class="pop-up-description">
+                <div>
+                    <p>${projects[i].description.big}
+                    </p>
+                </div>
+                <div class="pop-up-technologies">
+                    <ul class="categories">
+                       ${listOfelements}
+                    </ul>
+                    <hr>
+                    <div class="pop-up-btns">
+                    
+                        <a target="_blank" href="${projects[i].links[0]}" class="btn">See Live <img src="images/live-icon.svg"
+                                alt=""></a>
+                        <a target="_blank" href="${projects[i].links[1]}" class="btn">See Source <img src="images/github-blue-icon.svg"
+                                alt=""></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>`;
+
+    divPopup.innerHTML += popupText;
+    workSection.appendChild(divPopup);
+    const closePopup = document.querySelector('.close-popup');
+    closePopup.addEventListener('click', () => {
+      divPopup.style.display = 'none';
+      divPopup.innerHTML = '';
+    });
+  });
+});
